de6a7f03df6f3f751860c4e2b300000495841c62
painarasimha <pai.narasimha04@gmail.com>
2024-11-03 17:55:21 +0530

    sidebar design and routing functioning was added

---
+++ frontend/src/components/sidebar.svelte
@@ -0,0 +1,35 @@
+<script>
+  import { Link } from "svelte-routing";
+</script>
+
+<div
+  class="flex flex-col items-center py-10 bg-[#708090] text-foreground w-[260px] backdrop-blur-2xl h-screen rounded-br-xl shadow-lg shadow-foreground">
+    <a href="/"><span class="text-3xl font-black">PharmaCo</span></a>
+    <ul class="flex flex-col items-center pt-14 space-y-5">
+      <li class="text-2xl font-bold p-5 hover:scale-110">
+        <a href="/medicines" class="flex items-center space-x-1"
+          ><span>Medicine</span></a
+        >
+      </li>
+      <li class="text-xl font-bold p-5 hover:scale-110">
+        <a href="/supplier" class="flex items-center space-x-1"
+          ><span>Supplier</span></a
+        >
+      </li>
+      <li class="text-xl font-bold p-5 hover:scale-110">
+        <a href="/sales" class="flex items-center space-x-1"
+          ><span>Orders</span></a
+        >
+      </li>
+      <li class="text-xl font-bold p-5 hover:scale-110">
+        <a href="/inventory" class="flex items-center space-x-1"
+          ><span>Inventory</span></a
+        >
+      </li>
+      <li class="text-xl font-bold p-5 hover:scale-110">
+        <a href="/customer" class="flex items-center space-x-1"
+          ><span>Customer</span></a
+        >
+      </li>
+    </ul>
+</div>
