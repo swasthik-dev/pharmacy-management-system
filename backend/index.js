dd0f6ef48799cb762ad9e2e32923db7ec4eac0d0
painarasimha <pai.narasimha04@gmail.com>
2024-11-03 17:14:04 +0530

    tested API call for medicine fetching

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,17 @@ const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
+// List out all medicines in the Medicine Page
+app.get('/api/medicines', async(req, res) => {
+  try {
+    const medicines = await prisma.medicine.findMany();
+    console.log('medicines:',medicines)
+    res.json(medicines)
+  } catch (error) {
+    console.error('Error found', error);
+    res.status(500).json({error: 'Failed to fetch data'});
+  }
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
