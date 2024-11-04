17466b014d94cd11622ad53de280fb2e5eecf270
painarasimha <pai.narasimha04@gmail.com>
2024-11-04 13:55:07 +0530

    supplier page created

---
+++ frontend/src/pages/supplier.svelte
@@ -0,0 +1,30 @@
+<script>
+  
+</script>
+
+<main class="container mx-auto p-10">
+  <div class="p-6">
+    <h1 class="text-3xl font-bold mb-6 text-gray-800">Supplier Page</h1>
+
+    <!-- Search Filter -->
+
+    <!-- Medicine List -->
+      <table class="min-w-full table-auto bg-background shadow-md rounded-lg">
+        <thead>
+          <tr
+            class="bg-[#524A4A] text-foreground uppercase text-sm leading-normal"
+          >
+            <th class="py-3 px-6 text-left">Supplier ID</th>
+            <th class="py-3 px-6 text-left">Name</th>
+            <th class="py-3 px-6 text-left">Contact Number</th>
+            <th class="py-3 px-6 text-left">Address</th>
+          </tr>
+        </thead>
+        <tbody class="text-foreground text-sm font-light">
+          <tr>
+            <td>Hey</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</main>
